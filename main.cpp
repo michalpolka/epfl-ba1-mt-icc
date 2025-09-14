0e9047a87065cc91633a90edd52f4c2a38c78297
Michal Polka <michal@michalpolka.me>
2025-09-14 18:02:50 +0200

    Renamed main to welcome, added welcome message.

--- main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hi! This repo contains the solutions to the EPFL C++ MOOC created by Ronan Boulic and his colleagues, taught in first year of Mircoengineering." << std::endl;
-    return 0;
-}
---
+++ welcome.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int welcome() {
+    std::cout <<
+        "Hi! This repo contains the solutions to the EPFL C++ MOOC created by Ronan Boulic and his colleagues, taught in first year of Mircoengineering." << std::endl <<
+        "To run a file, change the function name to main. I haven't made something better yet." << std::endl
+    ;
+    return 0;
+}
