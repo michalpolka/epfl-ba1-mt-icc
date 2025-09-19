9e0d450e7c44d67acdd41e181795a1ed8c6af513
Michal Polka <michal@michalpolka.me>
2025-09-19 08:59:55 +0200

    added logical operators

--- module-2/1-if-statement.cpp
+++ module-2/1-if-statement.cpp
@@ -10,9 +10,9 @@ int ifStatement() {
     cout << "To test this if statement, input a number (hint: is your number nice?): ";
     cin >> userInput;
 
-    // is userInput equal to 69 OR ( || ) equal to 420?
+    // is userInput equal to 69?
 
-    if ( userInput == 69 || userInput == 420 ) {
+    if ( userInput == 69 ) {
         cout << userInput << " is nice." << endl; // if yes, output this
     } else {
         cout << userInput << " is not nice." << endl; // if no, output this
