327f08c3f0d8deca80152f4580719eeaf5555696
Michal Polka <michal@michalpolka.me>
2025-09-19 08:43:19 +0200

    added return;

--- module-2/1-if-statement.cpp
+++ module-2/1-if-statement.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int ifStatement() {
 
     // If statement
 
@@ -18,7 +18,6 @@ int main() {
         cout << userInput << " is not nice." << endl; // if no, output this
     }
 
-
-
+    return 0;
 
 }
