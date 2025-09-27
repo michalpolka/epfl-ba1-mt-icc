744272dc8bee19ced63f654074e020e4c05abd0c
Michal Polka <michal@michalpolka.me>
2025-09-27 09:17:41 +0200

    added for loop theory

---
+++ module-3/1-for-loops.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // Initialise variables
+    int x{0};
+    int y{1};
+
+    // Ask for a small and large number:
+    cout << "Please enter a number x: ";
+    cin >> x;
+
+    cout << "Please enter a number y (y>x): ";
+    cin >> y;
+
+    // Show all numbers between x,y:
+    for (int i{x}; i<=y ;++i) {
+        cout << i << endl;
+    }
+
+
+    return 0;
+}
